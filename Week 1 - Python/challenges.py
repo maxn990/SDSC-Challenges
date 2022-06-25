ee4fb4a88cf52fb44846b6b984b25f6f20b9be01
m990 <maxn990@gmail.com>
2022-06-25 15:09:48 -0700

    Week 1

---
+++ Week 1 - Python/challenges.py
@@ -0,0 +1,22 @@
+# Write a recursive function to print the Fibonacci sequence: f(n)=f(n-1)+f(n-2)
+
+# Create a dictionary to hold 4 items. Reverse the dictionary using a loop. Set the dictionary back to its original order using slicing.
+
+# Using a loop, an array to hold numbers 0 through 19. Print the array backwards (using a loop), print even numbers, print odd numbers. Print even numbers backwards, print odd numbers backwards.
+
+# Using a loop, convert the alphabet to ascii.
+
+# Create a function to determine if a number is prime. If so, return true. If not, return false. Then, using an if statement, print “The number is prime” if the function returns true and “The number is not prime” if the function return false.
+
+
+# x = [6, 7, 9, 2, 4, 9, 14, 3, 7, 4, 3, 8, 16, 20, 3, 2, 1, 14]
+# Find the largest number. Find the smallest number.
+
+# Print the following two dimensional array in matrix format:
+# [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# Use indexing to get the number 6.
+# Use indexing to get the number 2.
+# Use indexing to get the number 7.
+
+# x = [6, 7, 9, 2, 4, 9, 14, 3, 7, 4, 3, 8, 16, 20, 3, 2, 1, 14]
+# Use slicing to get indexes 5 to 9, then find the sum.
