fe35ae7a0f4fd8e89851a92c7d369e4847b62322
Max Norman <maxn990@icloud.com>
2022-06-25 15:59:03 -0700

    Update challenges.py

--- Week 1 - Python/challenges.py
+++ Week 1 - Python/challenges.py
@@ -1,24 +1,44 @@
-# Write a recursive function to print the Fibonacci sequence: f(n)=f(n-1)+f(n-2)
+# Write a recursive function to print the Fibonacci sequence at a given number: f(n)=f(n-1)+f(n-2)
 # f(1) = 1
 # f(2) = 1
 
-# Create a dictionary to hold 4 items. Reverse the dictionary using a loop. Set the dictionary back to its original order using slicing.
+def fibb(n):
+  return False
 
-# Using a loop, an array to hold numbers 0 through 19. Print the array backwards (using a loop), print even numbers, print odd numbers. Print even numbers backwards, print odd numbers backwards.
+print(fibb(8))
+print(fibb(4))
+print(fibb(1))
+
+# Create a dictionary to hold 4 items. Reverse the dictionary using a loop.
+
+
+# Using a loop, create an array to hold numbers 3 through 19. Print the array backwards (using a loop), print even numbers, print odd numbers. Print even numbers backwards, print odd numbers backwards.
 
 # Using a loop, convert the alphabet to ascii.
+abc = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'.split(" ")
 
-# Create a function to determine if a number is prime. If so, return true. If not, return false. Then, using an if statement, print “The number is prime” if the function returns true and “The number is not prime” if the function return false.
 
+# Create a function to determine if a positive number is prime. If so, return true. If not, return false. Then, using a turnary operator, print “The number is prime” if the function returns true and “The number is not prime” if the function return false.
+def isPrime(n):
+  return False
 
-# x = [6, 7, 9, 2, 4, 9, 14, 3, 7, 4, 3, 8, 16, 20, 3, 2, 1, 14]
+# Test list:
+test = [0, 1, 2, 3, 10, 17]
+
 # Find the largest number. Find the smallest number.
+x = [6, 7, 9, 2, 4, -3, 9, 14, 3, 7, 4, -6, -24, 3, 8, 16, 20, 3, 2, 1, 14]
+
 
 # Print the following two dimensional array in matrix format:
-# [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 # Use indexing to get the number 6.
 # Use indexing to get the number 2.
 # Use indexing to get the number 7.
 
-# x = [6, 7, 9, 2, 4, 9, 14, 3, 7, 4, 3, 8, 16, 20, 3, 2, 1, 14]
-# Use slicing to get indexes 5 to 9, then find the sum.
+x = [6, 7, 9, 2, 4, 9, 14, 3, 7, 4, 3, 8, 16, 20, 3, 2, 1, 14]
+# Use slicing to get indexes 5 through 9, then find the sum.
+
+
+
+# Write a function to move every list item forward by one, and move the last item to index 0
+x = [6, 7, 9, 2, 4, 9, 14, 3, 7, 4, 3, 8, 16, 20, 3, 2, 1, 14]
